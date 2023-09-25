c03c79c648f62887ae34720ddafe33f466721623
AntonZinchenko32 <zinchenko309@gmail.com>
2023-09-25 13:55:19 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,74 +1,65 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 
 import Section from './Section/Section';
 import Notification from './Notification/Notification';
 import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
 import Statistics from './Statistics/Statistics';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
+export const App = () => {
+  const [good, setGoodFeedBack] = useState(0);
+  const [neutral, setNeutralFeedBack] = useState(0);
+  const [bad, setBadFeedBack] = useState(0);
 
   // Функція обробки кліку по кнопці, для залишення зворотонього зв'язку ********
-  clickHandle = evt => {
+  const clickHandle = evt => {
     const whatClickedLowerCase = evt.target.textContent.toLowerCase();
 
-    this.setState(state => ({
-      [whatClickedLowerCase]: state[whatClickedLowerCase] + 1,
-    }));
+    if (whatClickedLowerCase === 'good') setGoodFeedBack(good + 1);
+    else if (whatClickedLowerCase === 'neutral')
+      setNeutralFeedBack(neutral + 1);
+    else setBadFeedBack(bad + 1);
   };
 
   // Функція підрахунку суми залишених відгуків
-  countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
-
+  const countTotalFeedback = () => {
     return good + bad + neutral;
   };
 
   // Функці підрахунку відсотку позитивних відгуків серед усіх відгуків
-  countPositiveFeedbackPercentage = () => {
-    return Math.round((this.state.good / this.countTotalFeedback()) * 100);
+  const countPositiveFeedbackPercentage = () => {
+    return Math.round((good / countTotalFeedback()) * 100);
   };
 
-  render() {
-    const { bad, good, neutral } = this.state;
-    const { clickHandle, countTotalFeedback, countPositiveFeedbackPercentage } =
-      this;
-
-    return (
-      <div
-        style={{
-          height: '100vh',
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          fontSize: 40,
-          color: '#010101',
-        }}
-      >
-        <Section title={'Please leave feedback'}>
-          <FeedbackOptions
-            options={Object.keys(this.state)}
-            onLeaveFeedback={clickHandle}
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101',
+      }}
+    >
+      <Section title={'Please leave feedback'}>
+        <FeedbackOptions
+          options={['good', 'neutral', 'bad']}
+          onLeaveFeedback={clickHandle}
+        />
+      </Section>
+      {countTotalFeedback() ? (
+        <Section title="Statistics">
+          <Statistics
+            bad={bad}
+            neutral={neutral}
+            good={good}
+            total={countTotalFeedback()}
+            positivePercentage={countPositiveFeedbackPercentage()}
           />
         </Section>
-        {countTotalFeedback() ? (
-          <Section title="Statistics">
-            <Statistics
-              bad={bad}
-              neutral={neutral}
-              good={good}
-              total={countTotalFeedback()}
-              positivePercentage={countPositiveFeedbackPercentage()}
-            />
-          </Section>
-        ) : (
-          <Notification message="There is no feedback" />
-        )}
-      </div>
-    );
-  }
-}
+      ) : (
+        <Notification message="There is no feedback" />
+      )}
+    </div>
+  );
+};
